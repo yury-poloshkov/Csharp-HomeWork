9b5fae3d44de39ddb01b69e4ad70f21655c85a94
Yury Poloshkov <1286600@gmail.com>
2022-12-15 23:54:07 +0300

    First task completed

--- Seminar1-HomeWork/Program.cs
+++ Seminar1-HomeWork/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# Seminar 1. HomeWork.
+
+// Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2) Console.WriteLine("Введенные числа одинаковые!");
+else
+{
+    int maximum = num1, minimum = num1;
+    if (num2 > maximum) maximum = num2;
+    else minimum = num2;
+    Console.WriteLine($"Из введенных чисел большее: {maximum}, меньшее: {minimum}.");
+}
