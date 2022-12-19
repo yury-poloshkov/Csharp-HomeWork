7f648eca4f887ba1dcaf01b7c1df8eb80b769006
Yury Poloshkov <1286600@gmail.com>
2022-12-20 01:24:01 +0300

    seminar 2 task 3 completed

--- Seminar2-HomeWork/Program.cs
+++ Seminar2-HomeWork/Program.cs
@@ -15,7 +15,7 @@ else Console.WriteLine($"Вторая цифра числа {num} - {GetSecondNu
 */
 
 // Task 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
+/*
 int GetThirdLeftNumber(int number)
 {
     int numberlength = 0, numbercopy = number;
@@ -33,3 +33,24 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (Math.Abs(num) < 100) Console.WriteLine($"В числе {num} нет третьей цифры.");
 else Console.WriteLine($"Третья цифра числа {num} - {GetThirdLeftNumber(Math.Abs(num))}.");
+*/
+
+// Task 3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+bool IsWeekEnd(int week_day_number)
+{
+    if (week_day_number < 6) return false;
+    else return true;
+}
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1 || num > 7) Console.WriteLine($"Стандартная неделя насчитывает 7 дней, а никак не {num}.");
+else 
+{
+    if (IsWeekEnd(num)) Console.WriteLine("Ура, выходной!!!");
+    else Console.WriteLine("Увы, придется идти на работу.");
+}
