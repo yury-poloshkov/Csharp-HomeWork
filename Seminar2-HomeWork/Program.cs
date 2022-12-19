47cc86f4a7bebdab6d6090590fe1fac4a254feac
Yury Poloshkov <1286600@gmail.com>
2022-12-19 22:07:14 +0300

    seminar 2 task 1 completed

--- Seminar2-HomeWork/Program.cs
+++ Seminar2-HomeWork/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# Seminar 2. HomeWork.
+// Task 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int GetSecondNumber(int number)
+{
+    return number / 10 % 10;   
+}
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999) Console.WriteLine($"Введенное число {num} не трехзначное.");
+else Console.WriteLine($"Вторая цифра числа {num} - {GetSecondNumber(num)}.");
