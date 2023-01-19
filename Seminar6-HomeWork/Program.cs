4c5c68bae716d1b0d3d780fa3259437b9e91a4b1
Yury.Poloshkov <1286600@gmail.com>
2023-01-20 00:32:58 +0300

    Semanar6.HomeWork (task 1 completed) initial commit

---
+++ Seminar6-HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿// C# Seminar 5. HomeWork.
+
+// Task 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int count_positive = 0;
+
+Console.Clear();
+Console.Write("Введите количество обрабатываемых чисел: ");
+int numbers_quantity = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= numbers_quantity; i++)
+{
+    Console.Write($"Введите {i}-е число: ");
+    if (Convert.ToInt32(Console.ReadLine()) > 0) count_positive++;
+}
+Console.WriteLine($"Количество положительных чисел: {count_positive}");
+
+// Task 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
